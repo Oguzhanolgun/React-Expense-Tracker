3dea94be2cb699e0cba953bac517037932c02ed1
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-05-05 18:53:19 +0300

    Adding NewExpense editing functionality (Method 2)

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,51 +2,37 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 import { useState } from "react";
 const NewExpense = (props) => {
-  const [showComponent, setShowComponent] = useState(false);
-  const [showBtn, setShowBtn] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
-    setShowComponent(false);
-    setShowBtn(true);
+    setIsEditing(false);
   };
 
-  const clickHandler = () => {
-    setShowComponent(true);
-    setShowBtn(false);
+  const startEditingHandler = () => {
+    setIsEditing(true);
   };
-  const cancelHandler = (e) => {
-    e.preventDefault();
-    setShowComponent(false);
-    setShowBtn(true);
-  };
-  const showForm = () => {
-    return (
-      <ExpenseForm
-        onSaveExpenseData={saveExpenseDataHandler}
-        onCancel={cancelHandler}
-      />
-    );
-  };
-  const showBtnHandler = () => {
-    return (
-      <button className="add_exp_btn" onClick={clickHandler}>
-        Add Expense
-      </button>
-    );
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      {/* <button className="add_exp_btn" onClick={clickHandler}>
-        Add Expense
-      </button> */}
-      {showComponent ? showForm() : null}
-      {showBtn ? showBtnHandler() : null}
-      {/* <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> */}
+      {!isEditing && (
+        <button className="add_exp_btn" onClick={startEditingHandler}>
+          Add New Expense
+        </button>
+      )}
+
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
