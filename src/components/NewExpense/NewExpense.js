1aadc402a8721df081ef7d6caa75a47c611c4e17
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-05-05 18:30:54 +0300

    Adding NewExpense editing functionality (Method 1)

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,16 +1,52 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 const NewExpense = (props) => {
+  const [showComponent, setShowComponent] = useState(false);
+  const [showBtn, setShowBtn] = useState(true);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setShowComponent(false);
+    setShowBtn(true);
   };
+
+  const clickHandler = () => {
+    setShowComponent(true);
+    setShowBtn(false);
+  };
+  const cancelHandler = (e) => {
+    e.preventDefault();
+    setShowComponent(false);
+    setShowBtn(true);
+  };
+  const showForm = () => {
+    return (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onCancel={cancelHandler}
+      />
+    );
+  };
+  const showBtnHandler = () => {
+    return (
+      <button className="add_exp_btn" onClick={clickHandler}>
+        Add Expense
+      </button>
+    );
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {/* <button className="add_exp_btn" onClick={clickHandler}>
+        Add Expense
+      </button> */}
+      {showComponent ? showForm() : null}
+      {showBtn ? showBtnHandler() : null}
+      {/* <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> */}
     </div>
   );
 };
